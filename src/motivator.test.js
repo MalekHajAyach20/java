d3e070bb11e1101337f1a419603ce0cbc68b01a0
MalekHajAyach20 <malekayech20@gmail.com>
2025-02-27 07:26:39 +0100

    erreur

--- src/motivator.test.js
+++ src/motivator.test.js
@@ -2,7 +2,10 @@ const getMotivationalPhrase = require("./motivator");
 
 test("La fonction retourne une phrase non vide", () => {
     const phrase = getMotivationalPhrase();
-    expect(phrase.length).toBeGreaterThan(0);
+    //expect(phrase.length).toBeGreaterThan(0);
+
+    expect(phrase.length).toBeLessThan(0);  // ERREUR VOLONTAIRE
+
 });
 
 //Ce test vérifie que la fonction retourne bien une phrase et qu’elle n’est pas vide.
