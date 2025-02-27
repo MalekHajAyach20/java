0555093706054059fa8126a4466d75940ec39d05
MalekHajAyach20 <malekayech20@gmail.com>
2025-02-27 07:37:45 +0100

    C DEL3

--- src/motivator.test.js
+++ src/motivator.test.js
@@ -4,8 +4,11 @@ test("La fonction retourne une phrase non vide", () => {
     const phrase = getMotivationalPhrase();
     expect(phrase.length).toBeGreaterThan(0);
 
-    //expect(phrase.length).toBeLessThan(0);  // ERREUR VOLONTAIRE
 
 });
 
 //Ce test vérifie que la fonction retourne bien une phrase et qu’elle n’est pas vide.
+
+
+
+
